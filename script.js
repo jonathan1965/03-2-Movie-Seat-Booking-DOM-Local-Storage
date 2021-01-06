bb74b73af7ff663de5343849f0c540f87c8a20ee
jonathan1965 <jonakarisa@gmail.com>
2021-01-07 00:08:53 +0200

    js all const

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const container = Document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count')
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document .getElementById('movieSelector');
